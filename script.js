4e1ed66755d354e974e9a7035ad277b0d95fab1e
plazazegknoffschwann-beep <plaza.zegknoff.schwann@gmail.com>
2026-03-05 21:56:29 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function greetUser() {
-    let name = document.getElementById("nameInput").value.trim();
-
-    if(name === "") {
-        document.getElementById("result").textContent = "Please enter your name.";
-    } else {
-        document.getElementById("result").textContent = "Hello, " + name + "! Welcome!";
-    }
-}
